c3f965a873dc53c937cc9002d4e3df3283910831
Michael Watson-Fore <michaelfore2587@gmail.com>
2021-12-31 13:39:27 -0700

    init commit

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, send_from_directory
+import os
+app = Flask(__name__)
+
+react_folder = 'flask-tutorial'
+directory= os.getcwd()+ f'/{react_folder}/build/static'
+
+
+@app.route('/')
+def index():
+    ''' User will call with with thier id to store the symbol as registered'''
+    path= os.getcwd()+ f'/{react_folder}/build'
+    print(path)
+    return send_from_directory(directory=path,path='index.html')
+
+#
+@app.route('/static/<folder>/<file>')
+def css(folder,file):
+    ''' User will call with with thier id to store the symbol as registered'''
+    
+    path = folder+'/'+file
+    return send_from_directory(directory=directory,path=path)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, threaded=True)
---
+++ flask-tutorial/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Hello World! I am being served from Flask!
+    </div>
+  );
+}
+
+export default App;
